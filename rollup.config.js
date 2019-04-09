929635e05ef41112d02e61a7f8f9cbbf136aa3eb
Sam Mason <smason@cloudflare.com>
2019-04-09 12:06:09 +0100

    Remove browser UMD version

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
 import babel from "rollup-plugin-babel"
 import sourceMaps from "rollup-plugin-sourcemaps"
 import { terser } from "rollup-plugin-terser"
@@ -7,37 +5,6 @@ import { sizeSnapshot } from "rollup-plugin-size-snapshot"
 import pkg from "./package.json"
 
 export default [
-  {
-    input: "src/use-cart.js",
-    external: ["react"],
-    output: {
-      name: "useCart",
-      sourcemap: true,
-      file: pkg.browser,
-      format: "umd",
-      globals: { react: "React" }
-    },
-    plugins: [
-      resolve(),
-      babel({
-        exclude: ["node_modules/**"]
-      }),
-      commonjs(),
-      sourceMaps(),
-      sizeSnapshot(),
-      terser({
-        sourcemap: true,
-        output: { comments: false },
-        compress: {
-          keep_infinity: true,
-          pure_getters: true
-        },
-        warnings: true,
-        ecma: 5,
-        toplevel: true
-      })
-    ]
-  },
   {
     input: "src/use-cart.js",
     external: ["react"],
